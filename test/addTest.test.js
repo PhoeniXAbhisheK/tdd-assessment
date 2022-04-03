66626a020dda1ae71bf4ce719fa7ffc18e61af2c
phoenixabhishek <abhiy3010@gmail.com>
2022-04-03 15:11:21 +0530

    TestCase: if input is blank, return zero. Refactor code to include common function to match i/o values.

--- test/addTest.test.js
+++ test/addTest.test.js
@@ -4,6 +4,11 @@ function addCalculator(inputVal){
   return inputVal === "" ? 0 : inputVal;
 }
 
+const checkConditions = (testValue, expectedResult) => {
+  const result = addCalculator(testValue);
+  expect(result).to.equal(expectedResult);
+};
+
 describe("input validation suite", function(){
   it("checks if function has input", () => {
     const result = addCalculator(1) != 0;
@@ -11,8 +16,7 @@ describe("input validation suite", function(){
   })
 
   it("checks if input is blank, return zero", () => {
-    const result = addCalculator("");
-    expect(result).to.equal(0);
+    checkConditions("", 0);
   })
 })
 
