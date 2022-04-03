a95f2adededa37ad7eff83ebff2d65629b356731
phoenixabhishek <abhiy3010@gmail.com>
2022-04-03 15:04:23 +0530

    TestCase: if input is blank, return zero. Refactor code.

--- test/addTest.test.js
+++ test/addTest.test.js
@@ -4,14 +4,16 @@ function addCalculator(inputVal){
   return inputVal === "" ? 0 : inputVal;
 }
 
-it("checks if function has input", () => {
-  const result = addCalculator(1) != 0;
-  expect(result).to.equal(true);
-})
+describe("input validation suite", function(){
+  it("checks if function has input", () => {
+    const result = addCalculator(1) != 0;
+    expect(result).to.equal(true);
+  })
 
-it("checks if input is blank", () => {
-  const result = addCalculator("");
-  expect(result).to.equal("");
+  it("checks if input is blank, return zero", () => {
+    const result = addCalculator("");
+    expect(result).to.equal(0);
+  })
 })
 
 it("checks if input is blank, return zero", () => {
