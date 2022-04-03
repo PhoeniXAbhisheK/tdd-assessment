46e36be89dafe019e3136d614f80f868e36ce8b0
phoenixabhishek <abhiy3010@gmail.com>
2022-04-03 14:47:40 +0530

    TestCase: if input is blank. Failing test case

--- test/addTest.test.js
+++ test/addTest.test.js
@@ -8,3 +8,8 @@ it("checks if function has input", () => {
   const result = addCalculator(1) != 0;
   expect(result).to.equal(true);
 })
+
+it("checks if input is blank", () => {
+  const result = addCalculator(1) != 0;
+  expect(result).to.equal("");
+})
